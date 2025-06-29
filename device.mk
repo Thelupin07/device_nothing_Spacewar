3c94591fdbe5002c8f1071526d1103683874c344
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-06-29 19:53:41 +0530

    Spacewar: Optimize dalvik heap config for performance
    Signed-off-by: abhixv <abhi.sharma1@hotmail.com>

--- device.mk
+++ device.mk
@@ -7,7 +7,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+PRODUCT_VENDOR_PROPERTIES += \
+    dalvik.vm.heapstartsize?=16m \
+    dalvik.vm.heapgrowthlimit?=384m \
+    dalvik.vm.heapsize?=512m \
+    dalvik.vm.heaptargetutilization?=0.75 \
+    dalvik.vm.heapminfree?=512k \
+    dalvik.vm.heapmaxfree?=8m
 
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
